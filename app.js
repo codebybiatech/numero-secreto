1351c0029d1d9c9801b5ab228984ab523239aba6
Beatriz Martins <beatriz.belo@bsatech.io>
2025-10-21 13:29:29 -0300

    atualizacao numero maximo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo!');
-let numeroMaximo = 20;
+let numeroMaximo = 100;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
