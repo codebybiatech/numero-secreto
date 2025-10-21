c29b0e6bd14ec24f6924c5adee4cd152ba72e696
codebybiatech <beatriz.belo@bsatech.io>
2025-10-21 11:29:31 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+alert('Boas vindas ao jogo!');
+let numeroMaximo = 20;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+//enquanto 
+while(chute != numeroSecreto){
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}!`);
+
+    if (chute == numeroSecreto) {
+        break;
+    } else if (chute > numeroSecreto) {
+        alert(`Voce errou! O número secreto é menor que ${chute}!`);
+    } else {
+        alert(`Voce errou! O número secreto é maior que ${chute}!`)
+    }
+    tentativas++;
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto}! com ${tentativas} ${palavraTentativa}`);
+
+// if (tentativas > 1){
+//     alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto}! com ${tentativas} tentativas`);
+// }else {
+//     alert(`Parabéns! Você descobriu o número secreto ${numeroSecreto}! com ${tentativas} tentativa`);
+
+// }
+
+
